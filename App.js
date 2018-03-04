c7c76aa47feefa6af026c934ed791a0e83275a3f
William Kammersell <william.kammersell@ca.com>
2018-03-04 06:46:34 -0700

    Randomize the color of the headers

--- App.js
+++ App.js
@@ -96,29 +96,31 @@ Ext.define('CustomApp', {
 			}
 		});
 		
+		var color = '#' + ( user.ObjectID % 1000000 ).toString().padStart( 6, '0' );
+		
 		//TODO: Handle null values
 		this.addLabel( detailsBox, user.FirstName + ' ' + user.MiddleName + ' ' + user.LastName );
 		this.addLabel( detailsBox, 'aka: ' + user.DisplayName );
 		
-		this.addHeader( detailsBox, 'Role' );
+		this.addHeader( detailsBox, 'Role', color );
 		this.addLabel( detailsBox, user.Role );
 		
-		this.addHeader( detailsBox, 'Office Location' );
+		this.addHeader( detailsBox, 'Office Location', color );
 		this.addLabel( detailsBox, user.OfficeLocation );
 		
-		this.addHeader( detailsBox, 'Email' );
+		this.addHeader( detailsBox, 'Email', color );
 		this.addLabel( detailsBox, user.EmailAddress );
 		
-		this.addHeader( detailsBox, 'Phone' );
+		this.addHeader( detailsBox, 'Phone', color );
 		this.addLabel( detailsBox, user.Phone );
 		
-		this.addHeader( detailsBox, 'Language' );
+		this.addHeader( detailsBox, 'Language', color );
 		this.addLabel( detailsBox, user.Language );
 		
-		this.addHeader( detailsBox, 'Default Project' );
+		this.addHeader( detailsBox, 'Default Project', color );
 		this.addLabel( detailsBox, user.DefaultProject._refObjectName );
 		
-		this.addHeader( detailsBox, 'Last Active Date' );
+		this.addHeader( detailsBox, 'Last Active Date', color );
 		this.addLabel( detailsBox, user.LastActiveDate.toLocaleString( 'en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' } ) );
 		
 		var imageBox = fullBox.add( {
@@ -141,13 +143,13 @@ Ext.define('CustomApp', {
     	//TODO: Add last work items
     },
     
-    addHeader:function( parent, text ) {
+    addHeader:function( parent, text, color ) {
 		parent.add( {
 			xtype: 'label',
 			html: '<u><b>' + text + '</b></u>',
 			style: {
-				'font-size': '10px',
-				'color': '#333333'
+				'font-size': '12px',
+				'color': color ? color : '#333333'
 			},
 			padding: '5 0 1 0'
 		} );
